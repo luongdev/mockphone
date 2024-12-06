0680ea5691a23584d0aa092bac379cb12d752fe2
__Luong <dot@luongmb.local>
2024-12-06 15:26:10 +0700

    abc

--- server/docker/lua/destroy.lua
+++ server/docker/lua/destroy.lua
@@ -1,25 +1,35 @@
 local cname = event:getHeader('Channel-Name') or '';
 local hup_cause = event:getHeader('Hangup-Cause') or 'NORMAL_CLEARING';
 
+if hup_cause == 'LOSE_RACE' then
+    local agent_uuid = event:getHeader('variable_cc_agent') or '';
+    if agent_uuid ~= '' then
+        local api = freeswitch.API();
+        api:executeString('callcenter_config agent set status ' .. agent_uuid .. " 'On Break'");
+    end
+
+    return ;
+end
+
 if string.sub(cname, 1, 7) == 'pickup/' and hup_cause ~= 'NORMAL_CLEARING' then
-  require 'base.curl';
+    require 'base.curl';
 
-  local uuid = event:getHeader('Unique-ID');
-  local global_call_id = event:getHeader('variable_global_call_id') or '';
-  local user = event:getHeader('variable_dialed_user') or '';
-  local domain = event:getHeader('variable_dialed_domain') or '';
-  local event_time_str = event:getHeader('Event-Date-Timestamp') or (tostring(os.time()) .. '000000');
+    local uuid = event:getHeader('Unique-ID');
+    local global_call_id = event:getHeader('variable_global_call_id') or '';
+    local user = event:getHeader('variable_dialed_user') or '';
+    local domain = event:getHeader('variable_dialed_domain') or '';
+    local event_time_str = event:getHeader('Event-Date-Timestamp') or (tostring(os.time()) .. '000000');
 
-  local url = 'http://10.196.26.15:3000/call/hangup';
-  local res = curl(url, 'post', {}, {
-      extension = user,
-      domain = domain,
-      type = 'HANGUP',
-      uuid = uuid,
-      globalCallId = global_call_id,
-      timeout = 3,
-      eventTime = math.floor(tonumber(event_time_str) / 1000)
-  }, 2);
+    local url = 'http://127.0.0.1:3333/call/hangup';
+    local res = curl(url, 'post', {}, {
+        extension = user,
+        domain = domain,
+        type = 'HANGUP',
+        uuid = uuid,
+        globalCallId = global_call_id,
+        timeout = 3,
+        eventTime = math.floor(tonumber(event_time_str) / 1000)
+    }, 2);
 
-  freeswitch.consoleLog('DBG', 'Push notification to ' .. url .. ' result ' .. tostring(res or '') .. '\n');
+    freeswitch.consoleLog('DBG', 'Push notification to ' .. url .. ' result ' .. tostring(res or '') .. '\n');
 end
--- server/docker/lua/pickup.lua
+++ server/docker/lua/pickup.lua
@@ -1,38 +1,47 @@
 local cname = event:getHeader('Channel-Name') or '';
 
 if string.len(cname) > 7 and string.sub(cname, 1, 7) == 'pickup/' then
-  require 'base.curl';
-  require 'base.getvar';
-  
-  local answer_state = event:getHeader('Answer-State') or '';
-  -- local presence_id = event:getHeader('Channel-Presence-ID') or '';
-  -- if answer_state ~= 'ringing' or presence_id == '' then return; end
-  if answer_state ~= 'ringing' then return; end
+    require 'base.curl';
+    require 'base.getvar';
+    local json = require 'resources.functions.lunajson';
 
-  local uuid = event:getHeader('Unique-ID') or '';
-  local backend_ip4 = event:getHeader('FreeSWITCH-IPv4') or '';
+    local answer_state = event:getHeader('Answer-State') or '';
+    -- local presence_id = event:getHeader('Channel-Presence-ID') or '';
+    -- if answer_state ~= 'ringing' or presence_id == '' then return; end
+    if answer_state ~= 'ringing' then
+        return ;
+    end
 
-  local api = freeswitch.API();
+    local uuid = event:getHeader('Unique-ID') or '';
+    local backend_ip4 = event:getHeader('FreeSWITCH-IPv4') or '';
 
-  local phone_number = event:getHeader('Caller-ANI') or '';
-  local user = getvar(uuid, 'dialed_user', '');
-  local domain = getvar(uuid, 'dialed_domain', '');
-  local event_time_str = event:getHeader('Event-Date-Timestamp') or (tostring(os.time()) .. '000000');
-  local glocal_call_id = getvar(uuid, 'global_call_id', '');
-  local leg_timeout = getvar(uuid, 'leg_timeout') or getvar(uuid, 'call_timeout', '10');
+    local phone_number = event:getHeader('Caller-ANI') or '';
+    local user = getvar(uuid, 'dialed_user', '');
+    local domain = getvar(uuid, 'dialed_domain', '');
+    local event_time_str = event:getHeader('Event-Date-Timestamp') or (tostring(os.time()) .. '000000');
+    --local glocal_call_id = getvar(uuid, 'global_call_id', '');
+    local leg_timeout = getvar(uuid, 'leg_timeout') or getvar(uuid, 'call_timeout', '10');
 
-  local url = 'http://10.196.26.15:3000/call/incoming';
-  local res = curl(url, 'post', {}, {
-      extension = user,
-      domain = domain,
-      type = 'INCOMING',
-      timeout = leg_timeout,
-      uuid = uuid,
-      backend = backend_ip4,
-      phoneNumber = phone_number,
-      globalCallId = global_call_id,
-      eventTime = math.floor(tonumber(event_time_str) / 1000)
-  }, 2);
+    local url = 'http://127.0.0.1:3333/call/incoming';
+    local res = curl(url, 'post', {}, {
+        extension = user,
+        domain = domain,
+        type = 'INCOMING',
+        timeout = leg_timeout,
+        uuid = uuid,
+        backend = backend_ip4,
+        phoneNumber = phone_number,
+        globalCallId = global_call_id,
+        eventTime = math.floor(tonumber(event_time_str) / 1000)
+    }, 2);
 
-  freeswitch.consoleLog('INFO', 'Push notification to ' .. url .. ' result ' .. tostring(res or '') .. '\n');
+    freeswitch.consoleLog('INFO', 'Push notification to ' .. url .. ' result ' .. tostring(res or '') .. '\n');
+
+    local api = freeswitch.API();
+    if string.len(res) < 2 then
+        api:executeString('uuid_kill ' .. uuid .. ' LOSE_RACE');
+    else
+        local table = json.decode(res);
+        if not table.success then api:executeString('uuid_kill ' .. uuid .. ' LOSE_RACE'); end
+    end
 end
